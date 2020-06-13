c421bd7262ce5d3c5f9af2040a5fd314975e7b06
nihalpandey4 <61798365+nihalpandey4@users.noreply.github.com>
2020-06-13 14:48:26 +0530

    using session id now instead of asking user explicitly

--- app.js
+++ app.js
@@ -4,99 +4,50 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(express.static("public"));
 const io = require("socket.io")(8080);
-var raisedHands = 0;
-var activeCount = 0;
-var users = [];
-
-const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/pollStatusDB",{useNewUrlParser:true,useUnifiedTopology:true});
-
-mongooseId="5ee33bfb07d3bd69c43ef90e";
-
-const status=new mongoose.Schema({
-  raised:Number,
-  activeUsers:Number,
-  userIds:[]
-});
-
-const Poll=mongoose.model("Poll",status);
-
-Poll.updateOne({_id:mongooseId},{raised:raisedHands,activeUsers:activeCount,userIds:users},function(err){
-  if(err){
-    console.log(err);
-  }
-});
-
+let count = 0;
+let activeCount = 0;
+let users = [];
 
 
 io.on("connection", socket => {
-
-
-  socket.on("new-window", id => {
-    updateUserids(id);
-  });
-
   socket.on("raise-hand", function() {
-    updateRaised(1);
+    count = count + 1;
+    console.log("Raise Hands " + (count/activeCount)*100+"%");
   });
 
   socket.on("hand-raised", function() {
-    updateRaised(-1);
+    count = count - 1;
+    console.log("Raise Hands " + (count/activeCount)*100+"%");
   });
+
 
 });
 
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+app.use(cookieParser());
+app.use(session({
+    secret: '34SDgsdgspxxxxxxxdfsG', // just a long random string
+    resave: false,
+    saveUninitialized: true
+}));
 
-function updateRaised(number){
-  raisedHands=raisedHands+number;
-  Poll.updateOne({_id:mongooseId},{raised:raisedHands},function(err){
-    if(err){
-      console.log(error);
-    }
-  });
-  pollStatus();
-}
+app.listen(3000,function(){
+  console.log("Server started at port 3000");
+});
 
-function updateActiveCount(number){
-  activeCount=activeCount+number;
-  Poll.updateOne({_id:mongooseId},{activeUsers:activeCount},function(err){
-    if(err){
-      console.log(error);
+app.get("/",function(req,res){
+  res.render("home.ejs");
+  const id= req.sessionID;
+  if (users.indexOf(id) == -1) {
+      users.push(id);
+      activeCount = activeCount + 1;
+      console.log("new user");
+    } else {
+      console.log("somebody returned");
     }
-  });
-}
-
-function updateUserids(newId){
-  Poll.findOne(function(err,polls){
-    if(err){
-      console.log("");
-    }
-    else{
-      users=polls.userIds;
-      if(users.indexOf(newId)==-1){
-        users.push(newId);
-        Poll.updateOne({_id:mongooseId},{userIds:users},function(err){
-          console.log("");
-        });
-        updateActiveCount(1);
-      }
-      else{
-        console.log("somebody returned");
-      }
-    }
-  });
-  pollStatus();
-}
-
-function pollStatus(){
-  Poll.findOne(function(err,poll){
-    if(err){
-      console.log(err);
-    }
-    else{
-      console.log("......................................");
-      console.log("Active Users = "+activeCount+"   percentage of people who raised their hands = "+(raisedHands/activeCount)*100+"%");
-    }
-  });
-}
+    console.log("Active users = "+activeCount);
+});
